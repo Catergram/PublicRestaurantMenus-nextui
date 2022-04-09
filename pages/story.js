b1c40e8f246d45312f92c0a880f17882866a5e5c
Jayraj Kapadia <jayraj@secretmindtech.com>
2022-04-09 18:49:01 +0530

    aaded clos button

--- pages/story.js
+++ pages/story.js
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from "next/router";
 import { Card, Col, Container, Grid } from '@nextui-org/react';
+
 import CardContent from "@components/CardContent";
 import { useMediaQuery } from '@components/useMediaQuery';
-import { useEffect, useState } from 'react';
 
 const images = [
   'https://unsplash.it/597/796?id=1',
@@ -12,6 +14,7 @@ const images = [
 ]
 
 export default function Story() {
+  const router = useRouter()
   const isMobile = useMediaQuery(650);
   const isTablet = useMediaQuery(1200);
   const isDesktop = useMediaQuery(1800);
@@ -36,60 +39,79 @@ export default function Story() {
   }, [active]);
 
   return (
-    <Grid.Container gap={2} justify="center" className='p-0 border-0 p-0 m-0 w-full'>
-      <Grid md={6} sm={12} justify="center" className="p-0"
-        css={{
-          height: isMobile ? '100vh' : '100vh'
-        }}
-      >
-        <Col css={isMobile ? {} : {
-          display: 'flex',
-          height: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: isTablet ? '0 18rem' : isDesktop ? '0 10rem' : '0 10rem'
+    <>
+      {!isMobile &&
+        <div className="flex justify-end cursor-pointer" onClick={() => {
+          router.push('/')
         }}>
-          <Card
-            css={
-              isMobile ? { height: '100vh', borderRadius: 0 }
-                :
-                { height: 500 }
-            }
-            cover
-            className="p-0 border-0"
-          >
-            <Card.Header css={{ position: "absolute", zIndex: 1, top: 0 }}>
-              <Col className="w-full">
-                <div className="slide-thumb w-full">
-                  <span />
-                  {
-                    images.map((image, key) => (
-                      <span
-                        key={key}
-                        className={`${key === active ? 'active' : ''}`}
-                      />
-                    ))
-                  }
-                </div>
-              </Col>
-            </Card.Header>
-            <Card.Image
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+        </div>
+      }
+      <Grid.Container gap={2} justify="center" className='p-0 border-0 p-0 m-0 w-full'>
+        <Grid md={6} sm={12} justify="center" className="p-0"
+          css={{
+            height: isMobile ? '100vh' : '100vh'
+          }}
+        >
+
+          <Col css={isMobile ? {} : {
+            display: 'flex',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: isTablet ? '0 18rem' : isDesktop ? '0 10rem' : '0 10rem'
+          }}>
+            <Card
               css={
-                isMobile ? { height: '100vh', borderRadius: 0 } :
-                  { height: '100%' }}
-              className={`border-0 h-full ${isMobile ? 'rounded-none' : ''}`}
-              src={images[active]}
-              width="100%"
-              alt="Card image background"
-            />
-            <Card.Footer css={{ position: "absolute", zIndex: 1, bottom: 5 }}>
-              <Col>
-                <CardContent storyButton={true} />
-              </Col>
-            </Card.Footer>
-          </Card>
-        </Col>
-      </Grid>
-    </Grid.Container >
+                isMobile ? { height: '100vh', borderRadius: 0 }
+                  :
+                  { height: 500 }
+              }
+              cover
+              className="p-0 border-0"
+            >
+
+              <Card.Header css={{ position: "absolute", zIndex: 1, top: 0 }}>
+                <Col className="w-full">
+
+                  <div className="slide-thumb w-full">
+                    <span />
+                    {
+                      images.map((image, key) => (
+                        <span
+                          key={key}
+                          className={`${key === active ? 'active' : ''}`}
+                        />
+                      ))
+                    }
+                  </div>
+                  {isMobile &&
+                    <div className="flex justify-end cursor-pointer" onClick={() => {
+                      router.push('/')
+                    }}>
+                      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                    </div>
+                  }
+                </Col>
+              </Card.Header>
+              <Card.Image
+                css={
+                  isMobile ? { height: '100vh', borderRadius: 0 } :
+                    { height: '100%' }}
+                className={`border-0 h-full ${isMobile ? 'rounded-none' : ''}`}
+                src={images[active]}
+                width="100%"
+                alt="Card image background"
+              />
+              <Card.Footer css={{ position: "absolute", zIndex: 1, bottom: 5 }}>
+                <Col>
+                  <CardContent storyButton={true} />
+                </Col>
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Grid>
+      </Grid.Container>
+    </>
   )
 }
