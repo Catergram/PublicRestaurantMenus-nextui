ac0267f5065e7480ee77765026bc111b13b8ada1
Jayraj Kapadia <jayraj@secretmindtech.com>
2022-04-12 11:44:32 +0530

    updated story page

--- pages/story.js
+++ pages/story.js
@@ -1,143 +1,111 @@
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import { Card, Grid, Button } from '@nextui-org/react'
-
-import Progress from '@components/Progress'
-import CardContent from '@components/CardContent'
-import { useMediaQuery } from '../hooks/useMediaQuery'
+import { useEffect, useState } from "react";
+import { Card, Grid } from "@nextui-org/react";
+import Progress from "../components/Progress";
+import CardContent from "../components/CardContent";
 
 const images = [
-  'https://unsplash.it/1920/1200?id=1',
-  'https://unsplash.it/1920/1200?id=2',
-  'https://unsplash.it/1920/1200?id=3',
-  'https://unsplash.it/1920/1200?id=4',
-  'https://unsplash.it/1920/1200?id=5',
-]
+  "https://dgh3t0irkf4qk.cloudfront.net/public/3ecbc810-0225-479e-9431-d80678da3c451648154823455",
+  "https://dgh3t0irkf4qk.cloudfront.net/public/81445abb-d2bf-49d3-ab1b-5916296b26571648154823454"
+  // "https://unsplash.it/567/500?id=3",
+  // "https://unsplash.it/567/500?id=4",
+  // "https://unsplash.it/567/500?id=5"
+];
 
 export default function Story() {
-  const router = useRouter()
-  const isMobile = useMediaQuery(650)
-  const isDesktop = useMediaQuery(2000)
-  const [active, setActive] = useState(0)
+  const [active, setActive] = useState(0);
 
   const activeSlide = (index) => {
-    setActive(index)
-  }
+    setActive(index);
+  };
 
   const next = () => {
     if (active < images.length - 1) {
-      activeSlide(active + 1)
+      activeSlide(active + 1);
     } else {
-      activeSlide(0)
+      activeSlide(0);
     }
-  }
+  };
 
   useEffect(() => {
-    let timer = setInterval(next, 5000)
-    return () => clearInterval(timer)
-  }, [active])
+    let timer = setInterval(next, 5000);
+    return () => clearInterval(timer);
+  }, [active]);
 
   return (
-    <div>
-      {
-        <Button
-          auto
-          color="error"
-          icon={<svg className="w-6 h-6" fill="black" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>}
-          css={{
-            position: 'fixed',
-            right: '10px',
-            top: isMobile ? '35px' : '10px',
-            color: 'black',
-            zIndex: '999'
-          }}
-          onClick={() => {
-            router.push('/')
-          }}
-        />
-      }
-      <Grid.Container
+    <Grid.Container
+      // gap={2}
+      css={{
+        "@xsMax": {
+          px: 0
+        },
+        width: "100%",
+        height: "600",
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'stretch'
+      }}
+    >
+      <Grid
+        md={6}
+        sm={12}
         css={{
-          '@xsMax': {
-            px: 0,
-          },
-          display: 'flex',
-          justifyContent: 'center',
-          height: '100vh',
-          alignItems: 'center',
+          "@xsMax": {
+            p: 0
+          }
         }}
       >
-        <Grid
-          md={6}
-          sm={12}
-          css={isMobile ? {} : {
-            '@xsMax': {
-              p: 0,
-              minHeight: '50vh',
-            },
-            display: 'flex',
-            justifyContent: 'center'
+        <Card
+          cover
+          css={{
+            "@xsMax": {
+              br: 0
+            }
           }}
         >
-          <Card
-            cover
+          <Card.Header
             css={{
-              '@xsMax': {
-                br: 0,
-                minHeight: '50vh',
-              },
-              width: isDesktop && '50vh',
-              height: isMobile ? '100vh' : 800,
-              borderRadius: isMobile && 0,
-              border: 'none'
+              position: "absolute"
             }}
           >
-            <Card.Header
-              css={{
-                position: 'absolute',
-              }}
-            >
-
-              {[...Array(images.length).keys()].map((key) => (
-                <Progress key={key} active={key === active} />
-              ))}
-            </Card.Header>
-            {images.map((image, key) => (
-              <Card.Image
-                key={key}
-                width={isDesktop ? '600' : '100%'}
-                height='100vh'
-                src={image}
-                active={key === active}
-                alt='Card image background'
-                containerCss={{
-                  d: key === active ? 'block' : 'none',
-                  '@xsMax': {
-                    br: 0,
-                    minHeight: '50vh',
-                  },
-                }}
-              />
+            {[...Array(images.length).keys()].map((key) => (
+              <Progress key={key} active={key === active} />
             ))}
-            <Card.Footer
-              css={{
-                zIndex: 1,
-                position: 'absolute',
-                p: '$md $lg',
-                bottom: 0,
-                left: 0,
-                background:
-                  'linear-gradient(0deg, rgba(0, 0, 0, 0.65) 0%, rgba(255, 255, 255, 0) 100%)',
-                '@xsMax': {
-                  p: '$lg $12',
-                },
+          </Card.Header>
+          {images.map((image, key) => (
+            <Card.Image
+              key={key}
+              width="100%"
+              height={600}
+              src={image}
+              active={key === active}
+              alt="Card image background"
+              containerCss={{
+                d: key === active ? "block" : "none",
+                "@xsMax": {
+                  br: 0,
+                  minHeight: "100vh"
+                }
               }}
-            >
-              <CardContent storyButton />
-            </Card.Footer>
-          </Card>
-        </Grid>
-      </Grid.Container>
-    </div >
-  )
+            />
+          ))}
+          <Card.Footer
+            css={{
+              zIndex: 1,
+              position: "absolute",
+              p: "$md $lg",
+              bottom: 0,
+              left: 0,
+              background:
+                "linear-gradient(0deg, rgba(0, 0, 0, 0.65) 0%, rgba(255, 255, 255, 0) 100%)",
+              "@xsMax": {
+                p: "$lg $12"
+              }
+            }}
+          >
+            <CardContent storyButton />
+          </Card.Footer>
+        </Card>
+      </Grid>
+    </Grid.Container>
+  );
 }
