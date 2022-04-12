58872ed45bbb35e90f66d19666d56590b311badb
Jayraj Kapadia <jayraj@secretmindtech.com>
2022-04-12 13:47:03 +0530

    set image in responsive

--- components/Footer/FooterCardLeft.js
+++ components/Footer/FooterCardLeft.js
@@ -1,4 +1,4 @@
-import { Card, Col, Grid, Row, Text } from "@nextui-org/react";
+import { Card, Col, Container, Grid, Row, Text } from "@nextui-org/react";
 import FooterMenuContent from "./FooterMenuContent";
 
 export default function FooterCardLeft({ isSm }) {
@@ -23,29 +23,26 @@ export default function FooterCardLeft({ isSm }) {
             </Col>
           </Card.Body>
           <Card.Body >
-            <Grid.Container justify="center">
-              <Row>
-                <Col span={12}>
-                  <Text
-                    size={30}
-                    weight="light"
-                    color="#8C8B8F"
-                  // style={{ textAlign: "left" }}
-                  >
-                    Millions of local menus. Share food with friends and discover new favorites.
-                  </Text>
-                </Col>
-                <Col span={6} style={{ margin: '0' }}>
-                  <Card.Image
-                    src="images/food.png"
-                    alt="Default Image"
-                    style={{ width: "320px", height: "150px" }}
-                    width={300}
-                    height={100}
-                  />
-                  {/* </Grid> */}
-                </Col>
-              </Row>
+            <Grid.Container gap={0} justify="center">
+              <Grid sm={6} md={8}>
+                <Text
+                  size={30}
+                  weight="light"
+                  color="#8C8B8F"
+                  style={{ textAlign: "left" }}
+                >
+                  Millions of local menus. Share food with friends and discover new favorites.
+                </Text>
+              </Grid>
+              <Grid sm={6} md={4} style={{ margin: '0' }}>
+                <Card.Image
+                  src="images/food.png"
+                  alt="Default Image"
+                  style={{ width: "320px", height: "150px" }}
+                  width={300}
+                  height={100}
+                />
+              </Grid>
             </Grid.Container>
           </Card.Body>
           {!isSm &&
