aee89d5892615c4891666e22e53f805bd0adcc81
Jayraj Kapadia <jayraj@secretmindtech.com>
2022-04-18 19:25:28 +0530

    remove all unwanted error

--- components/Layout.js
+++ components/Layout.js
@@ -287,10 +287,6 @@ export default function Layout({ children }) {
           crossOrigin=""
           href="/fonts/sf-pro-display/SF-Pro.ttf"
         />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        />
       </Head>
 
       {children}
